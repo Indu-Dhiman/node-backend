0aaa664cb9a499fb525f1a61ea609194d3596424
indu <indushah492@gmail.com>
2024-09-25 15:56:46 +0530

    Updated Code 25 September 2024

--- src/router/user.ts
+++ src/router/user.ts
@@ -1,4 +1,4 @@
-import { loginUser,createUser, updateUserProfile } from "../controllers/user/user";
+import { loginUser,createUser, updateUserProfile, forgotPassword, resetPassword } from "../controllers/user/user";
 import { Router } from "express";
 
 const router = Router();
@@ -7,6 +7,8 @@ router.post("/signup", createUser);
 
 router.post("/login", loginUser);
 router.post("/update-user-profile", updateUserProfile);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password", resetPassword);
 
 
 
